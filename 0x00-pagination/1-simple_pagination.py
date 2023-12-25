70a3f8e4f87ff26250a3dc92af9edc3c6632b3f1
IdiJr <idialijnr@gmail.com>
2023-12-25 15:30:14 +0000

    Simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -11,8 +11,7 @@ index_range = __import__('0-simple_helper_function').index_range
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """ Server class to paginate a database of popular baby names. """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -36,7 +35,6 @@ class Server:
         Args:
             page (int): The current page number.
             page_size (int): The number of items per page.
-
         Returns:
             List[List]: A list of rows corresponding to the
             requested page.
@@ -45,7 +43,12 @@ class Server:
         assert page > 0 and page_size > 0
 
         # Calculate start and end indexes using index_range function
-        start_index, end_index = index_range(page, page_size)
+        indices = index_range(page, page_size)
+        start = indices[0]
+        end = indices[1]
 
         # Return the appropriate page of the dataset
-        return self.dataset()[start_index:end_index]
+        try:
+            return self.dataset()[start:end]
+        except IndexError:
+            return []
