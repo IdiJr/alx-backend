5f863cf4f54e7001e9b360d5acccabbd87e0e1fe
IdiJr <idialijnr@gmail.com>
2024-01-10 15:41:50 +0000

    Basic Flask app

--- 0x02-i18n/routes/routes_0.py
+++ 0x02-i18n/routes/routes_0.py
@@ -8,13 +8,5 @@ app_routes = Blueprint('app_routes', __name__)
 
 @app_routes.route('/', methods=["GET"], strict_slashes=False)
 def home():
-    """
-    Route for the main page.
-
-    Returns:
-        str: Rendered HTML template.
-    """
-    return render_template(
-        '0-index.html', title='Welcome to Holberton',
-        header='Hello world'
-    )
+    """ Home page """
+    return render_template('0-index.html')
