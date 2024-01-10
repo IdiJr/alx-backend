3e7e653787c26d1cfd4c6e5339c46405a62f7913
IdiJr <idialijnr@gmail.com>
2024-01-10 09:38:48 +0000

    Force locale with URL parameter

---
+++ 0x02-i18n/routes/routes_4.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Basic route task 4"""
+from flask import Blueprint, render_template
+from flask_babel import _
+
+
+app_routes = Blueprint('app_routes', __name__)
+
+
+@app_routes.route('/', methods=["GET"], strict_slashes=False)
+def home():
+    """ Home page """
+    return render_template(
+        '4-index.html', title=_('home_title'),
+        header=_('home_header')
+    )
