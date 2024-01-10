0d792355d71c77a7c493f9b89fe07b90f87bfd55
IdiJr <idialijnr@gmail.com>
2024-01-10 15:17:21 +0000

    Mock logging in

--- 0x02-i18n/routes/routes_5.py
+++ 0x02-i18n/routes/routes_5.py
@@ -9,4 +9,7 @@ app_routes = Blueprint('app_routes', __name__)
 @app_routes.route('/', methods=["GET"], strict_slashes=False)
 def home():
     """ Home page """
-    return render_template('5-index.html')
+    return render_template(
+        '5-index.html', title=_('home_title'),
+        header=_('home_header')
+    )
