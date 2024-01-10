048b51daba8f523c09bfff9de005f0244ff62f87
IdiJr <idialijnr@gmail.com>
2024-01-10 09:33:15 +0000

    Get locale from request

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" 2-app module """
+from flask import Flask, request
+from flask_babel import Babel
+from routes.routes_2 import app_routes
+from config import Config
+
+
+app = Flask(__name__)
+
+# Instantiate Babel object
+babel = Babel(app)
+
+app.config.from_object(Config)
+app.register_blueprint(app_routes)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """ get_locale function to determine
+    the best match with supported languages
+    """
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
