02ede5667acc607452ea0066f977c85ad98d11d9
IdiJr <idialijnr@gmail.com>
2024-02-07 12:26:20 +0000

    Node Redis client and async operations

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,38 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+// Create a new Redis client
+const client = redis.createClient();
+
+// Promisify the get function of the Redis client
+const getAsync = promisify(client.get).bind(client);
+
+// Event listener for successful connection
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+// Event listener for connection error
+client.on('error', (error) => {
+  console.error(`Redis client not connected to the server: ${error}`);
+});
+
+// Function to set a new school value in Redis
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+// Function to display the value of a school key in Redis using async/await
+async function displaySchoolValue(schoolName) {
+  try {
+    const value = await getAsync(schoolName);
+    console.log(value);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Call the functions
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
